45d86cd79a84dd877a2aa7d21d06f5380c9e0cfa
s_zhuyfa <s_zhuyfa@jiedaibao.com>
2020-02-20 20:48:05 +0800

    jsp

--- src/main/java/com/seed/seed_manage/SeedManageApplication.java
+++ src/main/java/com/seed/seed_manage/SeedManageApplication.java
@@ -2,6 +2,9 @@ package com.seed.seed_manage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class SeedManageApplication {
@@ -10,4 +13,16 @@ public class SeedManageApplication {
         SpringApplication.run(SeedManageApplication.class, args);
     }
 
+    /**
+     * 添加对jsp支持
+     *
+     */
+    @Bean
+    public ViewResolver getJspViewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/jsp/");//前缀
+        internalResourceViewResolver.setSuffix(".jsp");//后缀
+        internalResourceViewResolver.setOrder(0);//优先级
+        return internalResourceViewResolver;
+    }
 }
