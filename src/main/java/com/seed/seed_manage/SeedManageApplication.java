13e28749f9abac91d69b570b706af2e5d1257cfa
Zhaozihan0823 <zhaozihan0823@163.com>
2020-02-20 19:47:45 +0800

    init

---
+++ src/main/java/com/seed/seed_manage/SeedManageApplication.java
@@ -0,0 +1,13 @@
+package com.seed.seed_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeedManageApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/seed/seed_manage/controller/JspController.java
@@ -0,0 +1,21 @@
+package com.seed.seed_manage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Classname:JspController
+ * Package:com.seed.seed_manage.controller
+ * Description:
+ *
+ * @DATE:2020/2/20 17:09
+ * @Author:zhaozihan0823
+ */
+@Controller
+public class JspController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
